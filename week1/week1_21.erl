683090ac0fa26d204ccbf8b43d5641792754e8a6
Dom <v.domanchuk@favbet.com>
2017-02-24 12:02:58 +0200

    add perfect func

--- week1/week1_21.erl
+++ week1/week1_21.erl
@@ -1,5 +1,5 @@
 -module(week1_21).
--export([fac/1, loop/1, fib/1]).
+-export([fac/1, loop/1, fib/1, perfect/1]).
 
 fac(N) ->
     fac(N, 1).
@@ -26,3 +26,18 @@ fib(2, _, ACCUM2) ->
 fib(N, ACCUM1, ACCUM2) ->
     fib(N-1, ACCUM2, ACCUM1 + ACCUM2).
 
+perfect(0) ->
+    false;
+perfect(1) ->
+    true;
+perfect(N) ->
+    perfect(N, 1, 0).
+
+perfect(N, CURR, ACCUM) when N =< CURR ->
+    N == ACCUM;
+
+perfect(N, CURR, ACCUM) when N rem CURR == 0 ->
+    perfect(N, CURR+1, ACCUM + CURR);
+
+perfect(N, CURR, ACCUM) when N rem CURR /= 0 ->
+    perfect(N, CURR+1, ACCUM).
