c8d650e6183b0bfe3380087365f96bc2960aa949
dom <domanchukits@gmail.com>
2017-02-26 20:48:25 +0200

    fix syntatic errors

--- week1/assignment.erl
+++ week1/assignment.erl
@@ -3,7 +3,7 @@
 
 % auxiliary func 
 getModuleOfVector({X1, Y1}, {X2, Y2}) ->
-    math:sqrt(math.pow(X1-X2)+math.pow(Y1-Y2)).
+    math:sqrt(math:pow(X1-X2)+math:pow(Y1-Y2)).
 
 % auxiliary func
 getSidesOfTriangle({_, {AX,AY}, {BX,BY}, {CX,CY}}) ->
@@ -18,8 +18,8 @@ area({rectangle, _, H,W}) ->
     H*W;
 area({triangle, {AX,AY}, {BX,BY}, {CX,CY}}) ->
     {AB, BC, AC} = getSidesOfTriangle({triangle, {AX,AY}, {BX,BY}, {CX,CY}}),
-    P = (AB+BC+AC)/2,
-    math:sqrt(P * (P-AB)(P-BC)(P-AC)).
+    P = (AB+BC+AC) / 2,
+    math:sqrt(P * (P-AB) * (P-BC) * (P-AC)).
 
 
 perimeter({circle, _, R}) ->
@@ -39,11 +39,14 @@ enclose({triangle, {AX,AY}, {BX,BY}, {CX,CY}}) ->
     A = lists:max([AX, BX, CX]), 
     B = lists:min([AX, BX, CX]),
     C = lists:max([AY, BY, CY]),
-    D = lists:min([AY, BY, CY])
+    D = lists:min([AY, BY, CY]),
     % sides of the rectangle
     H = A - B,
-    W = A - D,
+    W = C - D,
     % centre of the rectangle
     X = (A + B) / 2,
-    Y = (A + D) / 2,
+    Y = (C + D) / 2,
     {rectangle, {X,Y}, H, W}. 
+
+bits(N) ->
+    (N rem 2) + bits(N-1).
