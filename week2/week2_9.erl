10ec927d7ce6f730314dd10a6a7149df33528359
dom <domanchukits@gmail.com>
2017-02-27 22:20:05 +0200

    add median func

--- week2/week2_9.erl
+++ week2/week2_9.erl
@@ -1,5 +1,5 @@
 -module(week2_9).
--export([double/1, evens/1]).
+-export([double/1, evens/1, median/1]).
 
 % double
 double([]) -> 
@@ -12,6 +12,22 @@ evens([]) ->
     [];
 evens([X | Xs]) when X rem 2 == 0 ->
     [X | evens(Xs)];
-evens([X | Xs]) ->
+evens([_X | Xs]) ->
     evens(Xs).
 
+%sum aux
+sum([]) -> 0;
+sum([X | Xs]) -> X + sum(Xs).
+
+% median
+median(Xs) when length(Xs) rem 2 == 1 ->
+   median(Xs, 1, (length(Xs) div 2) + 1);
+median(Xs) ->
+   (median(Xs, 1, (length(Xs) div 2) + 1) + median(Xs, 1, length(Xs) div 2)) div 2.
+
+median([X | _Xs], CURR, FIND) when CURR == FIND ->
+    X;
+median([_X | Xs], CURR, FIND) ->
+median(Xs, CURR + 1, FIND).
+
+%make median with func composition sum!
