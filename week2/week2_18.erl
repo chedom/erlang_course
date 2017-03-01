65a8134cdd31bf21aef195e811e1bce4c2101188
Dom <v.domanchuk@favbet.com>
2017-03-01 15:35:15 +0200

    add split func

--- week2/week2_18.erl
+++ week2/week2_18.erl
@@ -1,6 +1,7 @@
 -module(week2_18).
--export([join/2, concat/1, member/2]).
+-export([join/2, concat/1, member/2, split/2]).
 
+% Joining lists together
 join([], Ys) ->
     Ys;
 join([X | Xs], Ys) ->
@@ -11,9 +12,31 @@ concat([]) ->
 concat([Xs | Xss]) ->
     join(Xs, concat(Xss)).
 
+% Testing membership
 member(_N, []) ->
     false;
 member(N, [N | _Xs]) ->
     true;
 member(N, [_X | Xs ]) ->
     member(N, Xs).
+
+
+% Sorting lists
+% ##########################################################
+
+% Merge sort ------------------------------------------------
+% merge_sort([]) ->
+%     [];
+% merge_sort([X]) ->
+%     [X];
+% merge_sort(XS) ->
+%     {}
+
+split(N, Xs) ->
+    split(N, Xs, []).
+split(_N, [], First) ->
+    {First, []};
+split(0, Xs, First) ->
+    { First, Xs };
+split(N, [X | Xs], First) ->
+    split(N - 1, Xs, join(First, [X])).
