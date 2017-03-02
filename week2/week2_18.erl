bf4fc7fa0ef4d4ba87913585d24335766e2f040b
Dom <v.domanchuk@favbet.com>
2017-03-02 12:03:35 +0200

    add quicksort and mergesort

--- week2/week2_18.erl
+++ week2/week2_18.erl
@@ -1,5 +1,5 @@
 -module(week2_18).
--export([join/2, concat/1, member/2, split/2]).
+-export([join/2, concat/1, member/2, merge_sort/1, quick_sort/1]).
 
 % Joining lists together
 join([], Ys) ->
@@ -25,12 +25,24 @@ member(N, [_X | Xs ]) ->
 % ##########################################################
 
 % Merge sort ------------------------------------------------
-% merge_sort([]) ->
-%     [];
-% merge_sort([X]) ->
-%     [X];
-% merge_sort(XS) ->
-%     {}
+merge_sort([]) ->
+    [];
+merge_sort([X]) ->
+    [X];
+merge_sort(Xs) ->
+    L = length(Xs),
+    {F, S} = split(L div 2, Xs),
+    merge(merge_sort(F), merge_sort(S)).
+
+merge([], Ys) ->
+    Ys;
+merge(Xs, []) ->
+    Xs;
+merge([X | Xs] = XS, [Y | Ys] = YS) ->
+    case X =< Y of
+        true -> [X | merge(Xs, YS)];
+        false -> [Y | merge(XS, Ys)]
+    end.
 
 split(N, Xs) ->
     split(N, Xs, []).
@@ -40,3 +52,20 @@ split(0, Xs, First) ->
     { First, Xs };
 split(N, [X | Xs], First) ->
     split(N - 1, Xs, join(First, [X])).
+
+% Quicksort sort ------------------------------------------------
+quick_sort([]) ->
+    [];
+quick_sort([X | Xs]) ->
+    quick_sort(filter(Xs, fun(Z) -> Z < X end)) ++ 
+    [X] ++ 
+    quick_sort(filter(Xs, fun(Z) -> Z >= X end)).
+
+
+filter([], _F) ->
+    [];
+filter([X | Xs], F) ->
+    case F(X) of    
+        true -> [X | filter(Xs, F)];
+        false -> filter(Xs, F)
+    end.
