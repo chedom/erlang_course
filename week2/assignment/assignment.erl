43c2458cb6054dac062dd52eb4b18bedf8155a59
Dom <v.domanchuk@favbet.com>
2017-03-03 11:56:37 +0200

    add processLine func

--- week2/assignment/assignment.erl
+++ week2/assignment/assignment.erl
@@ -1,14 +1,22 @@
 -module(assignment).
--export([test/0, splitWords/1]).
+-export([test/0, processLine/1]).
 -import(index, [get_file_contents/1]).
 
 test() ->
     get_file_contents("index.erl").
 
-flatMap([], _F) ->
+processLine(Xs) ->
+    filter(splitWords(map(fun nocap/1, Xs)), fun(X) -> length(X) > 2 end).
+
+flatMap(_F, []) ->
     [];
-flatMap([X | Xs], F) ->
+flatMap(F, [X | Xs]) ->
     F(X) ++ flatMap(Xs, F).
+
+map(_F, []) ->
+    [];
+map(F, [X | Xs]) ->
+    [F(X) | map(F, Xs)].
 
 member([], _X) ->
     false;
@@ -17,21 +25,26 @@ member([X | _Xs], X) ->
 member([_Y | Xs], X) ->
     member(Xs, X).
 
-
 splitWords(Str) ->
     splitWords(Str, [], []).
 splitWords([], Accum, Temp) ->
-    compact(Accum ++ [Temp]);
+    filter(Accum ++ [Temp], fun(X) -> X /= [] end);
 splitWords([C | Cs], Accum, Temp) ->
     case member(".,\ ;:\t\n\'-", C) of
         true -> splitWords(Cs, Accum ++ [Temp], []);
         false -> splitWords(Cs, Accum, Temp ++ [C])
     end.
 
-compact([]) ->
+nocap(X) ->
+    case $A=< X andalso X =< $Z of
+        true -> X + 32;
+        false -> X
+    end.
+
+filter([], _F) ->
     [];
-compact([X | Xs]) ->
-    case member([[], ""], X) of
-        true -> compact(Xs);
-        false -> [X | compact(Xs)]
+filter([X | Xs], F) ->
+    case F(X) of 
+        true -> [X | filter(Xs, F)];
+        false -> filter(Xs, F)
     end.
