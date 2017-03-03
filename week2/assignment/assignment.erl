19846daae6c696f056a6198bd5ae4abf6225c8e2
Dom <v.domanchuk@favbet.com>
2017-03-03 15:14:23 +0200

    add find undexes

--- week2/assignment/assignment.erl
+++ week2/assignment/assignment.erl
@@ -1,12 +1,26 @@
 -module(assignment).
--export([test1/0, test2/0, listToRange/1, processAllText/1]).
+-export([getIndexes/1]).
 -import(index, [get_file_contents/1]).
 
-test1() ->
-    findLines(transformLines(processAllText(get_file_contents("dickens-christmas.txt"))), "have").
+getIndexes(FileName) ->
+    NormalizedLines = normalizeLines(processAllText(get_file_contents(FileName))),
+    quick_sort(fun({W, _}) -> W end, makeIndexes(NormalizedLines, [])).
 
-test2() ->
-    get_file_contents("dickens-christmas.txt").
+
+makeIndexes([], Accum) ->
+    Accum;
+makeIndexes([{ _Num, Ys } | Xs] = Lines, Accum) ->
+    case Ys of
+        [] -> makeIndexes(Xs, Accum);
+        [Z | _Zs] ->
+            FindedLines = findLines(Lines, Z),
+            NewLines = deleteWordFromLines(Lines, Z, FindedLines),
+            makeIndexes(NewLines, Accum ++ [{Z, listToRange(FindedLines)}])
+    end.
+
+
+
+
 
 processAllText(Xs) ->
     map(fun processLine/1, Xs).
@@ -14,29 +28,6 @@ processAllText(Xs) ->
 processLine(Xs) ->
     filter(splitWords(map(fun nocap/1, Xs)), fun(X) -> length(X) > 2 end).
 
-flatMap(_F, []) ->
-    [];
-flatMap(F, [X | Xs]) ->
-    F(X) ++ flatMap(Xs, F).
-
-map(_F, []) ->
-    [];
-map(F, [X | Xs]) ->
-    [F(X) | map(F, Xs)].
-
-last([]) ->
-    badarg;
-last([X]) ->
-    X;
-last([_X | Xs]) ->
-    last(Xs).
-
-member([], _X) ->
-    false;
-member([X | _Xs], X) ->
-    true;
-member([_Y | Xs], X) ->
-    member(Xs, X).
 
 splitWords(Str) ->
     splitWords(Str, [], []).
@@ -47,6 +38,53 @@ splitWords([C | Cs], Accum, Temp) ->
         true -> splitWords(Cs, Accum, Temp ++ [C]);
         false -> splitWords(Cs, Accum ++ [Temp], [])
     end.
+
+
+normalizeLines(Xs) ->
+    normalizeLines(Xs, [], 1).
+normalizeLines([], Accum, _Num) ->
+    Accum;
+normalizeLines([X | Xs], Accum, Num) ->
+    normalizeLines(Xs, Accum ++ [{Num, X}], Num + 1).
+
+findLines([], _Y) ->
+    [];
+findLines([ {Num, Zs} | Xs], Y) ->
+    case member(Zs, Y) of   
+        true -> [Num | findLines(Xs, Y)];
+        false -> findLines(Xs, Y)
+    end.
+
+deleteWordFromLines(AllLines, Word, LinesForDel) ->
+    map(
+        fun({Num, Xs} = Line) ->
+            case member(LinesForDel, Num) of    
+                true -> {Num, filter(Xs, fun(Zs) -> Zs /= Word end)};
+                false -> Line
+            end
+        end,
+        AllLines
+    ).
+
+listToRange([]) ->
+    [];
+listToRange([X | Xs]) ->
+    listToRange(quick_sort(fun(Z) -> Z end , Xs), [], {X, X}).
+listToRange([], Accum, Temp) ->
+    Accum ++ [Temp];
+listToRange([X | Xs], Accum, {Start, End} = Range) ->
+    case (End + 1) == X of
+        true -> listToRange(Xs, Accum, {Start, X});
+        false -> listToRange(Xs, Accum ++ [Range], {X, X})
+    end.
+
+% Quicksort sort ------------------------------------------------
+quick_sort(_F, []) ->
+    [];
+quick_sort(F, [X | Xs]) ->
+    quick_sort(F, filter(Xs, fun(Z) -> F(Z) < F(X) end)) ++ 
+    [X] ++ 
+    quick_sort(F, filter(Xs, fun(Z) -> F(Z) >= F(X) end)).
 
 nocap(X) ->
     case $A=< X andalso X =< $Z of
@@ -62,37 +100,14 @@ filter([X | Xs], F) ->
         false -> filter(Xs, F)
     end.
 
-transformLines(Xs) ->
-    transformLines(Xs, [], 1).
-transformLines([], Accum, _Num) ->
-    Accum;
-transformLines([X | Xs], Accum, Num) ->
-    transformLines(Xs, Accum ++ [{Num, X}], Num + 1).
-
-findLines([], _Y) ->
+map(_F, []) ->
     [];
-findLines([ {Num, Zs} | Xs], Y) ->
-    case member(Zs, Y) of   
-        true -> [Num | findLines(Xs, Y)];
-        false -> findLines(Xs, Y)
-    end.
+map(F, [X | Xs]) ->
+    [F(X) | map(F, Xs)].
 
-listToRange([]) ->
-    [];
-listToRange([X | Xs]) ->
-    listToRange(quick_sort(Xs), [], {X, X}).
-listToRange([], Accum, Temp) ->
-    Accum ++ [Temp];
-listToRange([X | Xs], Accum, {Start, End} = Range) ->
-    case (End + 1) == X of
-        true -> listToRange(Xs, Accum, {Start, X});
-        false -> listToRange(Xs, Accum ++ [Range], {X, X})
-    end.
-
-% Quicksort sort ------------------------------------------------
-quick_sort([]) ->
-    [];
-quick_sort([X | Xs]) ->
-    quick_sort(filter(Xs, fun(Z) -> Z < X end)) ++ 
-    [X] ++ 
-    quick_sort(filter(Xs, fun(Z) -> Z >= X end)).
+member([], _X) ->
+    false;
+member([X | _Xs], X) ->
+    true;
+member([_Y | Xs], X) ->
+    member(Xs, X).
