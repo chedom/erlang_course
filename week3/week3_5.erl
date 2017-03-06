f598de19854b231f7ebb0cc3d746ca35d7d22d5c
Dom <v.domanchuk@favbet.com>
2017-03-06 13:52:15 +0200

    add zip_with func

--- week3/week3_5.erl
+++ week3/week3_5.erl
@@ -2,8 +2,11 @@
 -export([
     doubleAll/1, 
     evens/1, 
-    product/1]
-).
+    product/1,
+    zip/2,
+    zip_with/3,
+    zip_with_2/3
+]).
 
 doubleAll(Xs) ->
     lists:map(fun(X) -> X * 2 end, Xs).
@@ -13,3 +16,20 @@ evens(Xs) ->
     
 product(Xs) ->
     lists:foldr(fun(X, Acc) -> Acc * X end, 1, Xs).
+
+zip([], _Ys) ->
+    [];
+zip(_Xs, []) ->
+    [];
+zip([X | Xs], [Y | Ys]) ->
+    [{X, Y} | zip(Xs, Ys)].
+
+zip_with(_F, [], _Ys) ->
+    [];
+zip_with(_F, _Xs, []) ->
+    [];
+zip_with(F, [X | Xs], [Y | Ys]) ->
+    [F(X, Y) | zip_with(F, Xs, Ys)].
+
+zip_with_2(F, Xs, Ys) ->
+    lists:map(fun({X, Y}) -> F(X, Y) end, zip(Xs, Ys)).
