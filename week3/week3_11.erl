1f77c51bce7f9867452bbba83920617287447115
dom <domanchukits@gmail.com>
2017-03-06 21:45:14 +0200

    add iterate func

--- week3/week3_11.erl
+++ week3/week3_11.erl
@@ -1,5 +1,5 @@
 -module(week3_11).
--export([compose/1, twice/1]).
+-export([compose/1, twice/1, iterate/1]).
 
 compose(Xs) ->
     fun(Z) -> compose(Xs, Z) end.
@@ -11,3 +11,15 @@ compose([X | Xs], Z) ->
 
 twice(Func) ->
     fun(X) -> Func(Func(X)) end.
+
+id(X) -> 
+    X.
+
+iterate(N) ->
+    fun(F) -> iterate(N, F) end.
+
+iterate(0, _F) ->
+    fun id/1;
+iterate(N, F) ->
+    F(),
+    iterate(N - 1, F).
